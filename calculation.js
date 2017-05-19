31e6f78ffc7328bd056952b755fa9f71555fd034
chris <hayhay_co@yahoo.com>
2017-05-19 21:11:06 +0800

    adding js

---
+++ calculation.js
@@ -0,0 +1,42 @@
+window.onload = function()
+{
+  var keys = document.getElementsByTagName('button');
+
+  for (var i=0; i<keys.length; i++){
+    keys[i].onclick = function(){
+      var keyValue  = this.innerHTML;
+      detail        = document.getElementById('detail');
+      detailValue   = detail.innerHTML;
+      result        = document.getElementById('result-value')
+
+      switch (keyValue) {
+        case "C":
+          detail.innerHTML = "";
+          result.innerHTML = "0";
+          break;
+        case ":":
+          detail.innerHTML += this.innerHTML;
+          break;
+        case "*":
+          detail.innerHTML += this.innerHTML;
+          break;
+        case "-":
+          detail.innerHTML += this.innerHTML;
+          break;
+        case "+":
+          detail.innerHTML += this.innerHTML;
+          break;
+        case "=":
+          var lastChar = detail.indexOf(/.$/)
+
+          if 
+          result.innerHTML = eval(detail.innerHTML);
+          break;
+        default:
+          detail.innerHTML += this.innerHTML;
+      }
+
+      //
+    }
+  }
+}
